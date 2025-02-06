f651be1fb5f8b5b7d5c03530e3f80acbad895dc1
Mosfaqur Rahman <mosfaqur99@gmail.com>
2025-02-06 17:53:16 +0600

    JavaScript Task-1

---
+++ Task-1.js
@@ -0,0 +1,8 @@
+var taka = 1000;
+var orange = 300;
+var apple = 400;
+
+var totalCost = (orange + apple);
+var willBack = (taka - totalCost);
+
+console.log('Shop keeper will return'+' '+ willBack );
